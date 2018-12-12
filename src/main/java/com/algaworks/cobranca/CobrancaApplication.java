5a585fc55dce469233f92b4823f3a1ace836759f
Eduardo Moraes <eduardo.costa.info@gmail.com>
2018-12-12 20:43:50 -0200

    projeto finalizado - responsivo

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -20,4 +23,12 @@ public class CobrancaApplication {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 	
+	@Configuration
+	public static class mvcConfig extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+	}
 }
